e5b1b4c871cb6e6033fa824354a6462d6805e179
chandra <prakashniitpatna@gmail.com>
2020-07-08 16:38:06 +0530

    third comit

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,46 @@
 <template>
 <div class="container">
-  <div class="row mt-5">
-    <div class="col">
-      <h1 class="text-center">COVID-19 DATA</h1>
-    </div>
+<div class="row mt-5">
+  <div class="col">
+    <h1 class="text-center">COVID-19 DATA</h1>
   </div>
-  <div class="row mt-5" v-if="arrPositive.length > 0">
-    <div class="col">
-      <h2 class="text-center">Positive</h2>
-      <line-chart
-        :chartData="arrPositive"
-        :options="chartOptions"
-        :chartColors="positiveChartColors"
-        label="Positive"
-      />
-    </div>
+</div>
+<div class="row mt-5" v-if="arrPositive.length > 0">
+  <div class="col">
+    <h2 class="text-center">Positive</h2>
+    <line-chart
+      :chartData="arrPositive"
+      :options="chartOptions"
+      :chartColors="positiveChartColors"
+      label="Positive"
+    />
   </div>
+</div>
 
-  <div class="row mt-5" v-if="arrRecovered.length > 0">
-    <div class="col">
-      <h2 class="text-center">Recovered</h2>
-      <line-chart
-        :chartData="arrRecovered"
-        :options="chartOptions"
-        :chartColors="recoveredColors"
-        label="Recovered"
-      />
-    </div>
+<div class="row mt-5" v-if="arrRecovered.length > 0">
+  <div class="col">
+    <h2 class="text-center">Recovered</h2>
+    <line-chart
+      :chartData="arrRecovered"
+      :options="chartOptions"
+      :chartColors="recoveredColors"
+      label="Recovered"
+    />
   </div>
+</div>
 
-  <div class="row mt-5 mb-5">
-    <div class="col">
-      <h2 class="text-center">Deaths</h2>
-      <line-chart
-        v-if="arrDeaths.length > 0"
-        :chartData="arrDeaths"
-        :options="chartOptions"
-        :chartColors="deathColors"
-        label="Deaths"
-      />
-    </div>
+<div class="row mt-5 mb-5">
+  <div class="col">
+    <h2 class="text-center">Deaths</h2>
+    <line-chart
+      v-if="arrDeaths.length > 0"
+      :chartData="arrDeaths"
+      :options="chartOptions"
+      :chartColors="deathColors"
+      label="Deaths"
+    />
   </div>
+</div>
 </div>
 </template>
 
@@ -52,55 +52,54 @@ import LineChart from "./components/LineChart";
 
 export default {
 components: {
-  LineChart
+LineChart
 },
 data() {
-  return {
-    arrPositive: [],
-    positiveChartColors: {
-      borderColor: "#077187",
-      pointBorderColor: "#0E1428",
-      pointBackgroundColor: "#AFD6AC",
-      backgroundColor: "#74A57F"
-    },
-    arrRecovered: [],
-    recoveredColors: {
-      borderColor: "#4E5E66",
-      pointBorderColor: "#4E5E66",
-      pointBackgroundColor: "#31E981",
-      backgroundColor: "#31E981"
-    },
-    arrDeaths: [],
-    deathColors: {
-      borderColor: "#E06D06",
-      pointBorderColor: "#E06D06",
-      pointBackgroundColor: "#402A2C",
-      backgroundColor: "#402A2C"
-    },
-    chartOptions: {
-      responsive: true,
-      maintainAspectRatio: false
-    }
-  };
+return {
+  arrPositive: [],
+  positiveChartColors: {
+    borderColor: "#077187",
+    pointBorderColor: "#0E1428",
+    pointBackgroundColor: "#AFD6AC",
+    backgroundColor: "#74A57F"
+  },
+  arrRecovered: [],
+  recoveredColors: {
+    borderColor: "#4E5E66",
+    pointBorderColor: "#4E5E66",
+    pointBackgroundColor: "#31E981",
+    backgroundColor: "#31E981"
+  },
+  arrDeaths: [],
+  deathColors: {
+    borderColor: "#E06D06",
+    pointBorderColor: "#E06D06",
+    pointBackgroundColor: "#402A2C",
+    backgroundColor: "#402A2C"
+  },
+  chartOptions: {
+    responsive: true,
+    maintainAspectRatio: false
+  }
+};
 },
 async created() {
-  const { data } = await axios.get("https://covidtracking.com/api/us/daily");
+const { data } = await axios.get("https://covidtracking.com/api/us/daily");
 
-  data.forEach(d => {
-    const date = moment(d.date, "YYYYMMDD").format("MM/DD");
-    const {
-      positive,        
-      recovered,
-      death
-    } = d;
+data.forEach(d => {
+  const date = moment(d.date, "YYYYMMDD").format("MM/DD");
+  const {
+    positive,        
+    recovered,
+    death
+  } = d;
 
-    this.arrPositive.push({ date, total: positive });
-    this.arrRecovered.push({ date, total: recovered });
-    this.arrDeaths.push({ date, total: death });
-  });
+  this.arrPositive.push({ date, total: positive });
+  this.arrRecovered.push({ date, total: recovered });
+  this.arrDeaths.push({ date, total: death });
+});
 }
 };
 </script>
-
 <style>
 </style>
